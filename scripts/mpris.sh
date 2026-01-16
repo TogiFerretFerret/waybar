07bd351ccaf47c9f3e93996bd836918a1238a240
Riverxia <riverdalecoder@outlook.com>
2026-01-16 15:57:41 -0500

    kakasi

--- scripts/mpris.sh
+++ scripts/mpris.sh
@@ -4,7 +4,7 @@ PLAYER_STATUS=$(playerctl status 2>/dev/null)
 
 if [ "$PLAYER_STATUS" = "Playing" ] || [ "$PLAYER_STATUS" = "Paused" ]; then
     # Output the current track info
-    echo "$(playerctl metadata title) - $(playerctl metadata artist)"
+    echo "$(playerctl metadata title|kakasi -Ka -Ha -Ja) - $(playerctl metadata artist |kakasi -Ka -Ha -Ja)"
 else
     # Output custom text when nothing is playing/active
     echo "■ No media playing"
